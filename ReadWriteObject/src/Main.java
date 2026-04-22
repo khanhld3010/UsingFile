dbd12a1e93c8997fc97f9e76bcbf515118e123bc
khanhld3010 <luuduckhanh3010@gmail.com>
2026-04-22 15:53:20 +0700

    Read Write Object

---
+++ ReadWriteObject/src/Main.java
@@ -0,0 +1,45 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Student> students = new ArrayList<>();
+        students.add(new Student(1, "Vũ Kiều Anh", "Hà Nội"));
+        students.add(new Student(2, "Nguyễn Minh Quân", "Hà Nội"));
+        students.add(new Student(3, "Đặng Huy Hoà", "Đà Nẵng"));
+        students.add(new Student(4, "Nguyễn Khánh Tùng", "Hà Nội"));
+        students.add(new Student(5, "Nguyễn Khắc Nhật", "Hà Nội"));
+        writeDataToFile("student.txt", students);
+        List<Student> studentList = readDataFromFile("student.txt");
+        for (Student student : studentList){
+            System.out.println(student);
+        }
+    }
+
+    public static void writeDataToFile(String path, List<Student> students) {
+        try (FileOutputStream fileOutputStream = new FileOutputStream(path);
+             ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);) {
+            objectOutputStream.writeObject(students);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static List<Student> readDataFromFile(String path) {
+        List<Student> students = new ArrayList<>();
+        try {
+            FileInputStream fis = new FileInputStream(path);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            students = (List<Student>) ois.readObject();
+            fis.close();
+            ois.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return students;
+    }
+}
